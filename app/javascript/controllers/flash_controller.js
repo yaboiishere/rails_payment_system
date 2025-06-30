0015b4289e65c0e4caef04df5264c2ec7acc5079
Mihail Dobrev <mixaildobrev@gmail.com>
2025-06-30 22:24:46 +0300

    feat: make the flash messages to float

---
+++ app/javascript/controllers/flash_controller.js
@@ -0,0 +1,22 @@
+import {Controller} from "@hotwired/stimulus"
+
+export default class extends Controller {
+    connect() {
+        // Auto-dismiss after 5 seconds
+        this.dismissTimeout = setTimeout(() => {
+            this.close()
+        }, 5000)
+    }
+
+    disconnect() {
+        if (this.dismissTimeout) {
+            clearTimeout(this.dismissTimeout)
+        }
+    }
+
+    close() {
+        // Use Bootstrap's alert dismiss functionality
+        const bsAlert = bootstrap.Alert.getOrCreateInstance(this.element)
+        bsAlert.close()
+    }
+}
